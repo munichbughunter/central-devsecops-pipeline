7741f4b59dd64a3727228ffa241a216a8772c867
P.D. <munichbughunter@gmail.com>
2025-09-16 20:11:41 +0200

    Add Build and publish of a python base image

--- ci/build.go
+++ ci/build.go
@@ -2,6 +2,7 @@ package ci
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/munichbughunter/central-devsecops-pipeline/internal/dagger"
 )
@@ -13,4 +14,25 @@ func RunPythonBuild(ctx context.Context, client *dagger.Client, src *dagger.Dire
 		WithWorkdir("/src").
 		WithExec([]string{"python", "app.py"}).
 		Stdout(ctx)
+}
+
+// Build and publish the default Python image in one step
+func BuildDefaultPythonImageAndPublish(ctx context.Context, client *dagger.Client, imageTag string, githubUsername string, githubToken *dagger.Secret) (string, error) {
+	image := client.Container().
+		From("python:3.12-slim").
+		WithExec([]string{"pip", "install", "--no-cache-dir", "bandit", "black", "flake8"}).
+		WithLabel("maintainer", githubUsername).
+		WithUser("appuser").
+		WithWorkdir("/app")
+
+	// Reuse the generic publish function
+	return publishImage(ctx, client, image, imageTag, githubUsername, githubToken, "default-python")
+}
+
+// Publish any container image to a registry
+func publishImage(ctx context.Context, client *dagger.Client, image *dagger.Container, imageTag, githubUsername string, githubToken *dagger.Secret, repoName string) (string, error) {
+
+	return image.
+		WithRegistryAuth("ghcr.io", githubUsername, githubToken).
+		Publish(ctx, fmt.Sprintf("ghcr.io/%s/%s:%s", githubUsername, repoName, imageTag))
 }
